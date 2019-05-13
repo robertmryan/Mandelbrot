8cb95db40726fa475a9c51d45df7abf503ca69b9
Robert Ryan <robert.ryan@mindspring.com>
2019-05-13 10:47:09 -0700

    Initial Commit

---
+++ Mandelbrot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mandelbrot
+//
+//  Created by Robert Ryan on 5/13/19.
+//  Copyright © 2019 Robert Ryan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
